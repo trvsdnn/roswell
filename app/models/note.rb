4f7ed05638c1a0c55ea72a03beb67d72e29cd8f8
blahed <tdunn13@gmail.com>
2012-10-31 01:55:51 -0400

    add potentially slow map_reduce for pulling all tags of a given type

--- app/models/note.rb
+++ app/models/note.rb
@@ -6,4 +6,18 @@ class Note
   field :title, :type => String
   field :body, :type => String
   field :updated_by_ip, :type => String
+
+  def self.tags
+    _tags = []
+
+    Note.collection.map_reduce(Tag.map, Tag.reduce, :out => 'note_tags').find.each do |result|
+      next if result['value']['tags'].nil?
+
+      result['value']['tags'].each do |tag|
+        _tags << tag unless _tags.include?(tag)
+      end
+    end
+
+    _tags
+  end
 end
