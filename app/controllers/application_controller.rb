8c88511e1199472de0bcc59ddffb4e890414b619
blahed <tdunn13@gmail.com>
2012-11-02 20:04:46 -0400

    add filtering of tag navigation based on permission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,17 @@ class ApplicationController < ActionController::Base
   end
   helper_method :current_user
 
+  def allowed_tags_for(model)
+    klass = model.to_s[0..-2].camelize.constantize
+    tags = klass.tags
+    if current_user.admin?
+      tags
+    else
+      tags.reject { |tag| !current_user.allowed_tags.include?(tag) }
+    end
+  end
+  helper_method :allowed_tags_for
+
   def authorize
     if current_user.nil?
       redirect_to login_url, :alert => 'Not authorized'
