249245aff6d1f11e5cdd745c409d46ef3f3cb8b2
blahed <tdunn13@gmail.com>
2012-11-16 00:47:27 -0500

    expire session after 10 minutes

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Roswell::Application.config.session_store :cookie_store, key: '_roswell_session'
+Roswell::Application.config.session_store :cookie_store, key: '_roswell_session', :expire_after => 10.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
