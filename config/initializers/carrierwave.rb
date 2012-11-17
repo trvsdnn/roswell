34bec537b2b7b7c05d12c758d7141dc01ca4e194
Shuky Dvir <shuky@tooveo.com>
2012-11-18 00:37:55 +0200

    couple of small additions

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,13 +2,13 @@ CarrierWave.configure do |config|
 
   config.storage              = :grid_fs
   config.grid_fs_access_url   = "/files"
-  config.grid_fs_database     = Mongoid.database.name if Rails.env.development?
+  config.grid_fs_database     = Mongoid.database.name if Rails.env.development? or !ENV['HEROKU']
 
   if Rails.env.production? and ENV['HEROKU'] and (mongo = ENV['MONGOLAB_URI'] || ENV['MONGOHQ_URL'])
     settings = URI.parse(mongo)
     database_name = settings.path.gsub(/^\//, '')
 
-    config.grid_fs_database   = database_name || Mongoid.database.name
+    config.grid_fs_database   = database_name
     config.grid_fs_host       = settings.host
     config.grid_fs_port       = settings.port
     config.grid_fs_username   = settings.user
